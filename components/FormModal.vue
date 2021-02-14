5e54c5a496c36b36157df566877aeeab52ebf78b
Kawarabata <kwrbt0404@gmail.com>
2021-02-14 18:02:35 +0900

    create FormModal component

--- components/FormModal.vue
+++ components/FormModal.vue
@@ -1,5 +1,59 @@
 <template>
-  <div class="form-modal"></div>
+  <div class="form-modal" @click.self="$emit('close-modal')">
+    <div class="modal">
+      <input v-model="date" type="date" />
+      <input v-model="text" type="text" />
+      {{ date }}
+      {{ text }}
+      <button>貯める</button>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      date: '',
+      text: '',
+    }
+  },
+}
+</script>
+
+<style scoped lang="postcss">
+.form-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.64);
+
+  & .modal {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 36px 30px;
+    border: 5px solid #c4d700;
+    background-color: #fff;
+
+    & > * + * {
+      margin-top: 20px;
+    }
+  }
+
+  & button {
+    width: fit-content;
+    padding: 8px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+    background-color: #c4d700;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,20 @@
     <button @click="googleLogout">ログアウト</button>
     <title-header />
     <months-ribbon />
+    <button class="add-button" @click="openModal">
+      <img src="@/assets/images/button_add.svg" alt="追加" />
+    </button>
+    <form-modal v-if="isModalOpen" @close-modal="closeModal" />
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isModalOpen: false,
+    }
+  },
   computed: {
     isSignedIn() {
       return this.$store.state.me.isSignedIn
@@ -17,6 +26,12 @@ export default {
     googleLogout() {
       this.$store.dispatch('me/signOut')
     },
+    openModal() {
+      this.isModalOpen = true
+    },
+    closeModal() {
+      this.isModalOpen = false
+    },
   },
 }
 </script>
@@ -25,5 +40,11 @@ export default {
 .home {
   margin: 0 auto;
   min-height: 100vh;
+
+  & .add-button {
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+  }
 }
 </style>
